fa564ab7797165e56c67cef7f90339b2df61d467
FelixMo42 <felix@tacocat.com>
2019-10-08 13:21:53 -0700

    added jwt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express      = require('express')
+const bodyParser   = require('body-parser')
 const cookieParser = require('cookie-parser')
-const helmet = require('helmet')
-const cors = require('cors')
+const helmet       = require('helmet')
+const cors         = require('cors')
+const jwt          = require('jsonwebtoken')
+const uuid         = require("uuid/v4")
 
 const port = 3000
 
-let users = [
-    {username: "admin", password: "123"},
-    {username: "guest", password: "guest"}
-]
+const secret = "secret"
 
-let app = express()
+const app = express()
     .set('view engine', 'ejs')
     .use(express.static('public'))
     .use(bodyParser.json({}))
@@ -20,20 +19,45 @@ let app = express()
     .use(helmet())
     .use(cors())
 
+let users = [
+    {username: "admin", password: "123", secret: "admin secret"},
+    {username: "guest", password: "guest", secret: "guest secret"}
+]
+
 app.post("/login", (req, res) => {
     let username = req.body.username
     let password = req.body.password
 
-    if ( users.find((user) => user.username == username && user.password == password) ) {
-        res.cookie('logedIn' , true)
-        res.send({
-            success: true
-        })
-    } else {
+    let user = users.find((user) =>
+        user.username == username && user.password == password
+    )
+
+    if ( !user ) {
         res.send({
             success: false
         })
     }
+
+    //TODO: what if allready uid
+
+    user.uid = uuid()
+
+    let data = {
+        uid: user.uid,
+        username: username
+    }
+
+    let token = jwt.sign(data, secret, { expiresIn: "1h" })
+
+    res.cookie('session', token)
+
+    res.send({
+        success: true
+    })
+})
+
+app.get("/secret", (req, res) => {
+    jwt.verify()
 })
 
 app.listen(port)
